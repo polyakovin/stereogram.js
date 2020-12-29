7a8af87034cb76e15220f4128ce25bb8612df1f6
Igor Polyakov <web@polyakovin.ru>
2020-12-30 01:18:41 +0300

    initial commit after fork

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-stereogram.js
-=============
+# Autostereogram
 
-Javascript library to create Random Dot Stereogram
-
-This project is a port of openstereogram project at
-https://code.google.com/p/openstereogram/
-
-to Javascript
--- stereogram.js
+++ stereogram.js
@@ -1,304 +1,216 @@
-var ColorGenerator=(function(){
+(() => {
+  const createCanvasImage = (width, height) => {
+    const canvas = document.createElement('CANVAS');
+    canvas.width = width;
+    canvas.height = height;
+    return canvas;
+  };
 
-    function _colorGenerator(color1, color2, color3){
-	    if(arguments.length<1){
-		    this.colors=[0,0,0];
-		} else {
-	        this.colors=[color1,color2,color3];
-		}
-	}
-	
-    _colorGenerator.prototype.getRandomColor=function(){
-	     return this.colors[Math.floor(Math.random()*3)];
-	 }
+  const getImageData = (image) => {
+    const imageCtx = image.getContext('2d');
+    return imageCtx.getImageData(0, 0, image.width, image.height);
+  };
 
-	 
-	return  _colorGenerator;
-})();
+  const resizeImage = (image, width, height) => {
+    const newCanvas = createCanvasImage(width, height);
+    const newCanvasCtx = newCanvas.getContext('2d');
+    newCanvasCtx.drawImage(image, 0, 0, width, height);
+    return newCanvas;
+  }
 
+  const resizeTexturePattern = (image, maxSeparation) => {
+    const newHeight = (image.height * maxSeparation) / image.width;
+    return resizeImage(image, maxSeparation, newHeight);
+  }
 
-var UnbalancedColorGenerator=(function(){
+  const generateTextDepthMap = (
+    text,
+    fontSize,
+    width,
+    height,
+  ) => {
+    const textCanvas = createCanvasImage(width, height);
+    const textCanvasCtx = textCanvas.getContext('2d');
+    textCanvasCtx.font = `bold ${fontSize}pt Arial`;
+    textCanvasCtx.textAlign = 'center';
+    textCanvasCtx.fillStyle = '#000000';
+    textCanvasCtx.fillRect(0, 0, width, height);
+    textCanvasCtx.fillStyle = '#888888';
+    textCanvasCtx.fillText(text, width / 2, (height + fontSize) / 2);
+    return textCanvas;
+  }
 
-    function _unbalancedColorGenerator(color1, color2, color1Intensity){
-	    this.colors=[color1,color2];
-		this.color1Intensity = color1Intensity
-	}
-	
-    _unbalancedColorGenerator.prototype.getRandomColor=function(){
-	     return (Math.random()<this.color1Intensity)?this.colors[0]:colors[1];
-	 }
+  const getMinDepth = (
+    separationFactor,
+    maxDepth,
+    observationDistance,
+    suppliedMinDepth
+  ) => {
+    const computedMinDepth = Math.floor(
+      (separationFactor * maxDepth * observationDistance) /
+      (((1 - separationFactor) * maxDepth) + observationDistance)
+    );
+    return Math.min(
+      Math.max(computedMinDepth, suppliedMinDepth),
+      maxDepth,
+    );
+  }
 
-	 
-	return  _unbalancedColorGenerator;
-})();
+  const getMaxDepth = (
+    suppliedMaxDepth,
+    observationDistance,
+  ) => Math.max(
+    Math.min(suppliedMaxDepth, observationDistance),
+    0,
+  );
 
-var ImageManipulator=(function(){
+  const convertToPixels = (
+    valueInches,
+    ppi,
+  ) => Math.floor(valueInches * ppi);
 
-    function _imageManipulator(){}
-	
-    _imageManipulator.resizeDepthMap=function(image,width,height){
-	    var newCanvas = document.createElement('CANVAS');
-         newCanvas.width = width;
-         newCanvas.height = height;
-         var newCanvasCtx = newCanvas.getContext('2d');
-           newCanvasCtx.drawImage(image,0,0,width,height);
-          return newCanvas;		   
-	 }
-	 
-	 _imageManipulator.resizeTexturePattern=function(image,maxSeparation){
-	   //  if(image.width < maxSeparation){
-		     var newHeight = (image.height * maxSeparation)/image.width;
-			  var newCanvas = document.createElement('CANVAS');
-              newCanvas.width = maxSeparation;
-              newCanvas.height = newHeight;
-              var newCanvasCtx = newCanvas.getContext('2d');
-              newCanvasCtx.drawImage(image,0,0,maxSeparation,newHeight);
-              return newCanvas;
-		/* } else {
-		     var newCanvas = document.createElement('CANVAS');
-              newCanvas.width = image.width;
-              newCanvas.height = image.height;
-              var newCanvasCtx = newCanvas.getContext('2d');
-              newCanvasCtx.drawImage(image,0,0,newCanvas.width,newCanvas.height);
-              return newCanvas;
-		 }*/
-	 }
-	 
-	 _imageManipulator.generateTextDepthMap=function(text,fontSize,width,height){
-	     var textCanvas = document.createElement('CANVAS');
-		 textCanvas.width = width; textCanvas.height = height;
-		 var textCanvasCtx = textCanvas.getContext('2d');
-		 textCanvasCtx.font="bold "+fontSize+"pt Arial";
-		 textCanvasCtx.textAlign = 'center';
-		
+  const getDepth = (
+    depth,
+    maxDepth,
+    minDepth,
+  ) => Math.floor(maxDepth - (depth * (maxDepth - minDepth) / 255));
 
-		 textCanvasCtx.fillStyle="#000000";
-		 textCanvasCtx.fillRect(0,0,width,height);
-		 textCanvasCtx.fillStyle="#888888";
-		 textCanvasCtx.fillText(text,width/2,height/2);
-		 return textCanvas;
-	 }
+  const getSeparation = (
+    observationDistance,
+    eyeSeparation,
+    depth,
+  ) => Math.floor((eyeSeparation * depth) / (depth + observationDistance));
 
-	 
-	return  _imageManipulator;
-})();
+  const generateStereogram = (depthMap, texturePattern, {
+    width = 800,
+    height = 600,
+    observationDistanceInches = 14,
+    eyeSeparationInches = 3,
+    maxDepthInches = 12,
+    minDepthInches = 0,
+    horizontalPPI = 72,
+    verticalPPI = 72,
+    separationFactor = 0.55,
+  } = {}) => {
+    const observationDistance = convertToPixels(
+      observationDistanceInches,
+      horizontalPPI,
+    );
+    const eyeSeparation = convertToPixels(
+      eyeSeparationInches,
+      horizontalPPI,
+    );
+    const maxDepth = getMaxDepth(
+      convertToPixels(maxDepthInches, horizontalPPI),
+      observationDistance,
+    );
+    const minDepth = getMinDepth(
+      separationFactor,
+      maxDepth,
+      observationDistance,
+      convertToPixels(minDepthInches, horizontalPPI),
+    );
+    const verticalShift = Math.floor(verticalPPI / 16);
+    const maxSeparation = getSeparation(
+      observationDistance,
+      eyeSeparation,
+      maxDepth,
+    );
 
+    depthMap = resizeImage(depthMap, width, height);
+    const depthMapData = getImageData(depthMap);
 
+    texturePattern = resizeTexturePattern(texturePattern, maxSeparation);
+    const texturePatternData = getImageData(texturePattern);
 
-var StereogramGenerator =(function(){
+    const stereogram = createCanvasImage(width, height);
+    const stereogramCtx = stereogram.getContext('2d');
+    const stereogramData = getImageData(stereogram);
+    stereogramCtx.drawImage(texturePattern, 0, 0);
 
-    function _stereogramGenerator(){}
-	
-    _stereogramGenerator.generateSIRD=function(depthMap,color1,color2,
-	                                           color3,color1Intensity,
-											   width,height,observationDistanceInches,
-											   eyeSeparationInches,maxDepthInches,
-											   minDepthInches,horizontalPPI){
-											   
-        depthMap = ImageManipulator.resizeDepthMap(depthMap,width,height);
-		var depthMapCtx = depthMap.getContext('2d');
-		var depthMapData = depthMapCtx.getImageData(0,0,width,height);
-        var colors;
-        if(color3){
-		    colors = new ColorGenerator(color1,color2,color3);
+    const linksL = [];
+    const linksR = [];
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        linksL[x] = x;
+        linksR[x] = x;
+      }
+
+      for (let x = 0; x < width; x++) {
+        const depth = getDepth(
+          depthMapData.data[4 * (x + y * width)],
+          maxDepth,
+          minDepth,
+        );
+        const separation = getSeparation(
+          observationDistance,
+          eyeSeparation,
+          depth,
+        );
+        const left = Math.floor(x - (separation / 2));
+        const right = Math.floor(left + separation);
+
+        if (left >= 0 && right < width) {
+          let visible = true;
+
+          if (linksL[right] != right) {
+            if (linksL[right] < left) {
+              linksR[linksL[right]] = linksL[right];
+              linksL[right] = right;
+            } else {
+              visible = false;
+            }
+          }
+
+          if (linksR[left] != left) {
+            if (linksR[left] > right) {
+              linksL[linksR[left]] = linksR[left];
+              linksR[left] = left;
+            } else {
+              visible = false;
+            }
+          }
+
+          if (visible) {
+            linksL[right] = left;
+            linksR[left] = right;
+          }
+        }
+      }
+
+      let lastLinked = -10;
+      for (let x = 0; x < width; x++) {
+        const idx = 4 * (x + y * width);
+        const updateStereogramPixel = (fromIdx, from = stereogramData) => {
+          stereogramData.data[idx] = from.data[fromIdx];
+          stereogramData.data[idx + 1] = from.data[fromIdx + 1];
+          stereogramData.data[idx + 2] = from.data[fromIdx + 2];
+          stereogramData.data[idx + 3] = from.data[fromIdx + 3];
+        };
+
+        if (linksL[x] == x) {
+          if (lastLinked == x - 1) {
+            updateStereogramPixel(idx - 4);
+          } else {
+            const { width, height } = texturePattern;
+            updateStereogramPixel(
+              4 * ((x % maxSeparation) + width * (
+                Math.floor(y + ((x / maxSeparation) * verticalShift)) % height
+              )),
+              texturePatternData,
+            );
+          }
         } else {
-		    colors = new UnbalancedColorGenerator(color1,color2,color1Intensity);
-        }		
-	    var stereogram = document.createElement('CANVAS');
-        stereogram.width = width; stereogram.height=height;
-		var stereogramCtx = stereogram.getContext('2d');
-		stereogramCtx.drawImage(depthMap,0,0);
-		var stereogramData = stereogramCtx.getImageData(0,0,stereogram.width,stereogram.height);
-		
+          updateStereogramPixel(4 * (linksL[x] + y * width));
+          lastLinked = x;
+        }
+      }
+    }
 
-        var linksL = [];
-        var linksR = [];
-        var observationDistance = _stereogramGenerator.convertToPixels(observationDistanceInches,horizontalPPI);
-        var eyeSeparation = _stereogramGenerator.convertToPixels(eyeSeparationInches,horizontalPPI);
-        var maxdepth = 	_stereogramGenerator.getMaxDepth(_stereogramGenerator.convertToPixels(maxDepthInches,horizontalPPI),observationDistance);
-        var minDepth =  _stereogramGenerator.getMinDepth( 0.55, maxdepth, observationDistance, _stereogramGenerator.convertToPixels(minDepthInches, horizontalPPI) );
-		for(var l=0;l < height; l++){
-		    for(var c = 0;c<width;c++){
-			    linksL[c]=c;
-				linksR[c]=c;
-			}
-			for(var c=0; c < width; c++){
-			    var depth = _stereogramGenerator.obtainDepth(depthMapData.data[c*4+l*width*4+1],maxdepth,minDepth); 
-				var separation = _stereogramGenerator.getSeparation(observationDistance,eyeSeparation,depth);
-				var left = c - Math.floor(separation/2);
-				var right = Math.floor(left + separation);
-				if(left >=0 && right < width ){
-				    var visible = true;
-					if(linksL[right] != right){
-					    if(linksL[right] < left){
-						    linksR[linksL[right]] = linksL[right];
-							linksL[right] = right;
-						} else {
-						    visible = false;
-						}
-					}
-					if(linksR[left] != left){
-					    if(linksR[left] > right){
-						    linksL[linksR[left]] = linksR[left];
-							linksR[left] = left;
-						} else {
-						    visible = false;
-						}
-					}
-					if(visible){
-					    linksL[right] = left;
-						linksR[left] = right;
-					}
-				}
-			}
-			for(var c=0;c < width; c++){
-			    var idx = c*4+stereogram.width*4*l;
-			    if(linksL[c] == c){
-				     var randomColor = colors.getRandomColor();
-				     stereogramData.data[idx] = randomColor[0];
-					 stereogramData.data[idx+1] = randomColor[1];
-					 stereogramData.data[idx+2] = randomColor[2];
-					 
- 				} else {
-				     var idx2 = linksL[c]*4+l*stereogram.width*4;
-				     stereogramData.data[idx] = stereogramData.data[idx2];
-					 stereogramData.data[idx+1] = stereogramData.data[idx2+1];
-					 stereogramData.data[idx+2] = stereogramData.data[idx2+2];
-					 stereogramData.data[idx+3] = stereogramData.data[idx2+3];
-					 console.log(""+l+","+c);
-				}
-			}
-		}
+    stereogramCtx.putImageData(stereogramData, 0, 0);
+    return stereogram;
+  }
 
-		stereogramCtx.putImageData(stereogramData,0,0);
-		return stereogram;
-		
-	 }
-	 
-	 _stereogramGenerator.getMinDepth=function(separationFactor, maxdepth, observationDistance, suppliedMinDepth){
-	     var computedMinDepth = Math.floor( (separationFactor * maxdepth * observationDistance) /(((1 - separationFactor) * maxdepth) + observationDistance));
-		 return Math.min( Math.max( computedMinDepth, suppliedMinDepth), maxdepth);    
-	 }
-	 
-	 _stereogramGenerator.getMaxDepth=function(suppliedMaxDepth, observationDistance){
-	     return Math.max( Math.min( suppliedMaxDepth, observationDistance), 0);
-	 }
-
-	_stereogramGenerator.convertToPixels=function(valueInches,ppi){
-	     return Math.floor(valueInches * ppi);
-	}
-	
-	_stereogramGenerator.obtainDepth=function(depth, maxDepth, minDepth){
-	     return maxDepth - (depth * (maxDepth - minDepth) / 255);
-	}
-	
-	_stereogramGenerator.getSeparation=function(observationDistance,eyeSeparation,depth){
-	     return (eyeSeparation * depth) / (depth + observationDistance);
-	}
-	
-	_stereogramGenerator.generateTexturedSIRD=function( depthMap, texturePattern,
-			width, height,
-			observationDistanceInches, eyeSeparationInches,
-			maxDepthInches, minDepthInches,
-			horizontalPPI, verticalPPI){
-	      depthMap = ImageManipulator.resizeDepthMap(depthMap,width,height);
-		  var depthMapCtx = depthMap.getContext('2d');
-		  var depthMapData = depthMapCtx.getImageData(0,0,width,height);
-          var stereogram = document.createElement('CANVAS');
-          stereogram.width = width; stereogram.height=height;
-		  var stereogramCtx = stereogram.getContext('2d');
-
-		  var stereogramData = stereogramCtx.getImageData(0,0,stereogram.width,stereogram.height);
-		    var linksL = [];
-        var linksR = [];
-        var observationDistance = _stereogramGenerator.convertToPixels(observationDistanceInches,horizontalPPI);
-        var eyeSeparation = _stereogramGenerator.convertToPixels(eyeSeparationInches,horizontalPPI);
-        var maxDepth = 	_stereogramGenerator.getMaxDepth(_stereogramGenerator.convertToPixels(maxDepthInches,horizontalPPI),observationDistance);
-        var minDepth =  _stereogramGenerator.getMinDepth( 0.55, maxDepth, observationDistance, _stereogramGenerator.convertToPixels(minDepthInches, horizontalPPI) );
-		
-		var verticalShift = Math.floor(verticalPPI /16);
-		var maxSeparation = Math.floor(_stereogramGenerator.getSeparation(observationDistance, eyeSeparation, maxDepth));
-		
-		texturePattern = ImageManipulator.resizeTexturePattern( texturePattern, maxSeparation );
-		var texturePatternData = texturePattern.getContext('2d').getImageData(0,0,texturePattern.width,texturePattern.height);
-		stereogramCtx.drawImage(texturePattern,0,0);
-		
-		for ( var l = 0; l < height; l++ ) {
-			for ( var c = 0; c < width; c++ ) {
-				linksL[c] = c;
-				linksR[c] = c;
-			}
-			
-			for ( var c = 0; c < width; c++ ) {
-				var depth = Math.floor(_stereogramGenerator.obtainDepth( depthMapData.data[c*4+4*l*depthMap.width], maxDepth, minDepth ));
-				var separation = Math.floor(_stereogramGenerator.getSeparation(observationDistance, eyeSeparation, depth))
-				var left = Math.floor(c - (separation / 2));
-				var right = Math.floor(left + separation);
-				
-				if ( left >= 0 && right < width ) {
-					var visible = true;
-					
-					if ( linksL[right] != right) {
-						if ( linksL[right] < left) {
-							linksR[linksL[right]] = linksL[right];
-							linksL[right] = right;
-						}
-						else {
-							visible = false;
-						}
-					}
-					if ( linksR[left] != left) {
-						if ( linksR[left] > right) {
-							linksL[linksR[left]] = linksR[left];
-							linksR[left] = left;
-						}
-						else {
-							visible = false;
-						}
-					}
-					
-					if ( visible ) {
-						linksL[right] = left;
-						linksR[left] = right;
-					}					
-				}
-			}
-			
-			var lastLinked = -10;
-			for (var c = 0; c < width; c++) {
-			    var idx = 4*c + 4*l*width;
-				if ( linksL[c] == c ) {
-					if (lastLinked == c - 1) {
-						stereogramData.data[idx] = stereogramData.data[idx-4];
-						stereogramData.data[idx+1] = stereogramData.data[idx-4+1];
-						stereogramData.data[idx+2] = stereogramData.data[idx-4+2];
-						stereogramData.data[idx+3] = stereogramData.data[idx-4+3];
-					}
-					else {
-					    var textureIdx = (c % maxSeparation)*4 + (Math.floor(l + ((c / maxSeparation) * verticalShift)) % texturePattern.height)*texturePattern.width*4;
-						stereogramData.data[idx] = texturePatternData.data[textureIdx];
-						stereogramData.data[idx+1] = texturePatternData.data[textureIdx+1];
-						stereogramData.data[idx+2] = texturePatternData.data[textureIdx+2];
-						stereogramData.data[idx+3] = texturePatternData.data[textureIdx+3];;
-					}
-				}
-				else {
-				    var idx2 = linksL[c]*4 + 4*l*width;
-						stereogramData.data[idx] = stereogramData.data[idx2];
-						stereogramData.data[idx+1] = stereogramData.data[idx2+1];
-						stereogramData.data[idx+2] = stereogramData.data[idx2+2];
-						stereogramData.data[idx+3] = stereogramData.data[idx2+3];
-
-					lastLinked = c;
-				}
-			}
-		}
-		
-		stereogramCtx.putImageData(stereogramData,0,0);
-		return stereogram;
-	}
-	 
-	return  _stereogramGenerator;
-})(); 
-
-
+  window.generateTextDepthMap = generateTextDepthMap;
+  window.generateStereogram = generateStereogram;
+})();
